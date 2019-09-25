46387e7bd59df94918fb4507d4a698bab5de2919
julyseng <julyseng@gmail.com>
2019-09-25 18:52:44 +1200

    clean up folder

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const express = require('express')
 
-const redditRoutes = require('./reddit')
+// const journeyRoutes = require('./journey')
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.json())
 
-server.use('/api/v1/reddit', redditRoutes)
+// server.use('/api/v1/journey', journeyRoutes)
 
 module.exports = server
--- tests/server/reddit.test.js
+++ tests/server/reddit.test.js
@@ -3,16 +3,16 @@ import request from 'supertest'
 
 import server from '../../server/server'
 
-test('GET /api/v1/reddit/subreddit', () => {
-  const scope = nock('http://www.reddit.com')
-    .get('/r/bananas.json')
-    .reply(200, {data: {children: {msg: 'yay, bananas'}}})
+// test('GET /api/v1/reddit/subreddit', () => {
+//   const scope = nock('http://www.reddit.com')
+//     .get('/r/bananas.json')
+//     .reply(200, {data: {children: {msg: 'yay, bananas'}}})
 
-  return request(server)
-    .get('/api/v1/reddit/subreddit/bananas')
-    .expect(200)
-    .then(res => {
-      scope.done()
-      expect(res.body.msg).toBe('yay, bananas')
-    })
-})
+//   return request(server)
+//     .get('/api/v1/reddit/subreddit/bananas')
+//     .expect(200)
+//     .then(res => {
+//       scope.done()
+//       expect(res.body.msg).toBe('yay, bananas')
+//     })
+// })
