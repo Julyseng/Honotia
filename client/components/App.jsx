b5b09217cfe2e4b9cd04a8d95fbace00aafabd02
student <student@pop-os.localdomain>
2019-09-25 20:41:31 +1200

    start registration form

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
-const App = () => (
-  <div className='app'>
-  hello world
-  </div>
-)
+import Registration from './Registration'
+
+
+const App = () => {
+  return (
+    <Router>
+      <div className='app'>
+      hello world
+      <Registration />
+      </div>
+
+      <Route path="/registration" component={Registration}/>
+
+  </Router>
+  )
+}
 
 export default App
