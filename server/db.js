7f8a6c65e93a32f0e8ac8cd0e5000baff3fad8b1
student <student@pop-os.localdomain>
2019-09-26 11:46:20 +1200

    data reaches db.js file

--- server/db.js
+++ server/db.js
@@ -1,8 +1,21 @@
-
 const env = process.env.NODE_ENV || 'development'
 const config = require('../knexfile')[env]
 const connection = require('knex')(config)
 
+module.exports = {
+    postUserInfoDb
+}
+
 function postUserInfoDb(data, db=connection) {
-    return db()
+    return db('tableName')
+        .insert({
+            name: data.name,
+            age: data.age,
+            languages: data.languages,
+            location: data.location,
+            occupation: data.occupation,
+            interests: data.interests,
+            support: data.support,
+            email: data.email
+        })
 }
