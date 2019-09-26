dd494fcec9fa61111d15729295326b3b9acbe35e
Chloe <45995939+chloeseun@users.noreply.github.com>
2019-09-26 12:46:40 +1200

    remove console.log

--- client/apiClient.js
+++ client/apiClient.js
@@ -1,7 +1,6 @@
 import request from 'superagent'
 
 export function postUserInfo(data) {
-    console.log(data)
     return request.post('/api/v1/journey/postUserInfo')
         .send(data)
         .catch(e => { console.log(e) })
