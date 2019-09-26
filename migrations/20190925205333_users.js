dcfcc5b64f2b43303af36de544a50cde2f07f900
julyseng <julyseng@gmail.com>
2019-09-26 19:48:10 +1200

    create fake data in seed

--- migrations/20190925205333_users.js
+++ migrations/20190925205333_users.js
@@ -2,13 +2,14 @@
 exports.up = function(knex) {
     return knex.schema.createTable('users', t => {
         t.increments('id')
-        t.string('first-name')
-        t.string('last-name')
+        t.string('first_name')
+        t.string('last_name')
         t.date('birthday')
         t.string('current_city')
         t.string('occupation')
         t.string('interests')
         t.string('email')
+        t.string('password')
         t.string('bio')
         t.string('user_type')
     })
---
+++ seeds/user_language.js
@@ -0,0 +1,19 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('user_languages').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('user_languages').insert([
+        {language_id: 1, user_id: 4},
+        {language_id: 2, user_id: 3},
+        {language_id: 2, user_id: 6},
+        {language_id: 4, user_id: 1},
+        {language_id: 3, user_id: 5},
+        {language_id: 5, user_id: 3},
+        {language_id: 5, user_id: 2},
+        {language_id: 6, user_id: 2},
+        
+      ]);
+    });
+};
