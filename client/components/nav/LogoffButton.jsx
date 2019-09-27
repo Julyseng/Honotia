592017adef9b6a262e38ac14408820db7f5fe2b6
chloe <chloeseunpark@gmail.com>
2019-09-27 21:05:08 +1200

    rearrange menu items layout

--- client/components/nav/LogoffButton.jsx
+++ client/components/nav/LogoffButton.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 
 export default function LogoffButton({ handleLogoff }) {
   return (
-    <li>
       <a
         className='waves-effect waves-light btn-large btn-round'
         onClick={handleLogoff}
       >
         Log Off
       </a>
-    </li>
   )
 }
