9c8210166afbcd839c6a4e7b590fd7d392cfc0e0
julyseng <julyseng@gmail.com>
2019-09-28 19:15:43 +1200

    setting up

--- client/components/AboutPage.jsx
+++ client/components/AboutPage.jsx
@@ -4,6 +4,17 @@ export default function AboutPage() {
   return (
     <div>
       <h2>About</h2>
+      <h4>What is Honotia</h4>
+      <p>
+        Honotia means the action of connection in Te Reo Maori. 
+      </p>
+
+      <h4>Why it exist</h4>
+      <p>
+        Coming to New Zealand or moving to a new 
+      </p>
+
+      <h4>How to Register</h4>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum dicta
         repudiandae aliquam labore placeat, praesentium nam reprehenderit,
@@ -13,6 +24,9 @@ export default function AboutPage() {
         natus quam eum doloribus culpa odit ratione nesciunt temporibus,
         voluptatum ipsum dignissimos perspiciatis sapiente eveniet.
       </p>
+
+      <h4>Meet The Team</h4>
+      
     </div>
   )
 }
