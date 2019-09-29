a23a0f4479571953f6815c16f20b1689bb3e869f
julyseng <julyseng@gmail.com>
2019-09-29 22:57:36 +1300

    add content

--- client/components/AboutPage.jsx
+++ client/components/AboutPage.jsx
@@ -4,29 +4,45 @@ export default function AboutPage() {
   return (
     <div>
       <h2>About</h2>
-      <h4>What is Honotia</h4>
-      <p>
-        Honotia means the action of connection in Te Reo Maori. 
-      </p>
+      <h4> Honotia means the action of connection in Te Reo Maori.</h4>
+        <p>Since 1956 till today, New Zealand accepts refugees from all over the world.
+          In 2018, refugee quota has increased to 1000 per year. Every refugee spends
+          the first six weeks in Mangere Refugee Resettlement Centre where they are
+          supported with English Language classes, health screening and mental health.
+          Moving to a new country and starting all over is not easy. You are either excited,
+          nervous or overwhelmed stepping into a foreign land without knowing where to begin.
+          Although there are many supports available after the six weeks programme, they are
+          scattered all over the place and individual support are either limited or non-existence.
+        </p>
 
-      <h4>Why it exist</h4>
-      <p>
-        Coming to New Zealand or moving to a new 
-      </p>
+        <p> If you are a refugee who just arrived in New Zealand, we would like to help you connect
+          with other refugees or Ally who are interested to support you and make your journey a
+          little easier. Honotia is a place where you find allies and former refugees for the
+          continued support and build your network of people.
+        </p>
 
-      <h4>How to Register</h4>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum dicta
-        repudiandae aliquam labore placeat, praesentium nam reprehenderit,
-        aspernatur assumenda obcaecati dolor. Id tenetur illum debitis illo
-        inventore sint aut repudiandae. Lorem ipsum dolor, sit amet consectetur
-        adipisicing elit. Dolorem sequi doloremque, cum eaque adipisci amet
-        natus quam eum doloribus culpa odit ratione nesciunt temporibus,
-        voluptatum ipsum dignissimos perspiciatis sapiente eveniet.
-      </p>
+      <h4>Our vision</h4>
+        <p>
+          To connect, educate and empower refugees and people of New Zealand
+        </p>
+
+      <h4>Our mission</h4>
+        <p>
+          Creating a cohesive community where everyone can learn from each other’s
+          stories and support each other.
+        </p>
+
+      <h4>Register now and start making connection</h4>
+
+      Step 1. Choose a status - Newly Arrived Refugee, Former Refugee, Ally (Non Refugee background) 
+      Step 2. Fill out a form 
+      Step 3. View profiles 
+      Step 4. Connect
+
+
 
       <h4>Meet The Team</h4>
-      
+
     </div>
   )
 }
