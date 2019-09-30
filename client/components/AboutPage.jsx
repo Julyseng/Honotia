856df0a52319f518b11a8721104ee2667edc9661
julyseng <julyseng@gmail.com>
2019-09-30 16:56:48 +1300

    styling page

--- client/components/AboutPage.jsx
+++ client/components/AboutPage.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export default function AboutPage() {
   return (
     <div className='about-container'>
-      <h3 className='about-h3'>What is refugee according to UNHCR</h3>
+      <h4 className='about-h4'>What is refugee </h4>
       <h4 className='quotation'>  “A person forced to flee their country </h4>
-      <h4 className='quotation'>  because of violence or persecution.”  </h4>
+      <h4 className='quotation'>  because of violence or persecution.”  - UNHCR </h4>
 
-      <h4 className='about-h3'> Honotia means </h4>
+      <h4 className='about-h4'> Honotia means </h4>
       <h4 className='quotation'> “the action of connection” </h4>
-      <br/>
-      
+      <br />
+
       <p>Since 1956 till today, New Zealand accepts refugees from all over the world.
         In 2018, refugee quota has increased to 1000 per year. Every refugee spends
         the first six weeks in Mangere Refugee Resettlement Centre where they are
@@ -26,9 +26,9 @@ export default function AboutPage() {
         little easier. Honotia is a place where you find allies and former refugees for the
         continued support and build your network of people.
       </p>
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
 
       <div className='box-wrapper'>
         <div className='about-box'>
@@ -61,77 +61,54 @@ export default function AboutPage() {
         </div>
       </div>
 
-      {/* <h4>Register now and start making connection</h4>
-      <ul>
-        <li> Step 1. Choose a status </li>
-        <br/>
-        <br/>
-        <li> Step 2. Fill out a form </li>
-        <br/>
-        <br/>
-        <li> Step 3. View profiles </li>
-        <br/>
-        <br/>
-        <li> Step 4. Connect </li>
-      </ul> */}
+      <section className="block-content t-block-teal l-block-spacing">
+        <div className="l-contained">
+          <header className="heading-group">
+            <h3 className='about-h3'>How to start connecting</h3>
+          </header>
+          <ul className="timeline-list">
+            <li>
+              <div className="content">
+                <h3> Step 1:</h3>
 
-<section className="block-content t-block-teal l-block-spacing">
-					<div className="l-contained">
-						<header className="heading-group">
-							<h3 className='about-h3'>How to start connecting</h3>
-							<p className="subtitle">
-								What is this?
-							</p>
-						</header>
-						<ul className="timeline-list">
-							<li>
-                  <div className="content">
-								<h3>A timeline?</h3>
-						
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.
+                <p>
+                Choose your status. If you are a newly arrived refugee, your status
+                can be updated later too.
 								</p>
-                </div>
-							</li>
-							<li>
-                  <div className="content">
-								<h3>A railway map?</h3>
-							
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.
-								</p>
-                </div>
-							</li>
-							<li>
-                  <div className="content">
-								<h3>Random dots?</h3>
-							
-						
-								<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.
-								</p>
-                </div>
-							</li>
-							
+              </div>
+            </li>
+            <li>
+              <div className="content">
+                <h3> Step 2:</h3>
 
-							
-							<li>
-                  <div className="content">
-								<h3>Absolutely nothing.</h3>
-								
-						
-								<p>
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.
+                <p>
+                  Register on the page to fill out your details and let other people know 
+                  abit about yourself
 								</p>
-                </div>
-							</li>
-						</ul>
-						
-								
-						
-					</div>
+              </div>
+            </li>
+            <li>
+              <div className="content">
+                <h3> Step 3: </h3>
+                <p>
+                  View your profile and profiles to learn about other user on the 
+                  page.
+								</p>
+              </div>
+            </li>
 
-				</section>
+            <li>
+              <div className="content">
+                <h3> Step 4:</h3>
+                <p>
+                  Take action and start connecting and enjoy your journey.
+								</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+      </section>
 
     </div >
   )
