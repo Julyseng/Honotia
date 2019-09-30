0f625cf525ac12604527cbf3ea89319ddeef7090
julyseng <julyseng@gmail.com>
2019-09-30 16:11:15 +1300

    add timeline

--- client/components/AboutPage.jsx
+++ client/components/AboutPage.jsx
@@ -75,6 +75,64 @@ export default function AboutPage() {
         <li> Step 4. Connect </li>
       </ul> */}
 
+<section className="block-content t-block-teal l-block-spacing">
+					<div className="l-contained">
+						<header className="heading-group">
+							<h3 className='about-h3'>How to start connecting</h3>
+							<p className="subtitle">
+								What is this?
+							</p>
+						</header>
+						<ul className="timeline-list">
+							<li>
+                  <div className="content">
+								<h3>A timeline?</h3>
+						
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.
+								</p>
+                </div>
+							</li>
+							<li>
+                  <div className="content">
+								<h3>A railway map?</h3>
+							
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.
+								</p>
+                </div>
+							</li>
+							<li>
+                  <div className="content">
+								<h3>Random dots?</h3>
+							
+						
+								<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.
+								</p>
+                </div>
+							</li>
+							
+
+							
+							<li>
+                  <div className="content">
+								<h3>Absolutely nothing.</h3>
+								
+						
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quam felis, rutrum nec enim non, sodales facilisis purus. Vestibulum viverra egestas ipsum eget commodo. Nullam aliquet lorem vitae nulla dictum vestibulum sed quis tellus. Sed diam diam, facilisis tincidunt volutpat nec, auctor quis magna. Proin sed nunc iaculis ipsum scelerisque tincidunt. Cras eleifend leo tellus, fermentum finibus tortor fringilla eu.
+								</p>
+                </div>
+							</li>
+						</ul>
+						
+								
+						
+					</div>
+
+				</section>
+
     </div >
   )
 }
