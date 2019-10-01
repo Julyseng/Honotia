1dce42e7e16c134c26b2ed15d221f9b3c9a48c5b
student <student@pop-os.localdomain>
2019-10-02 10:10:16 +1300

    write first 2 actions tests

--- tests/client/actions.test.js
+++ tests/client/actions.test.js
@@ -2,18 +2,39 @@ import nock from 'nock'
 
 import * as actions from '../../client/actions'
 
-test.skip('fetchPosts', () => {
+test('fetchFormDatas', () => {
   const scope = nock('http://localhost')
-    .get('/api/v1/reddit/subreddit/bananas')
-    .reply(200, [{data: 'yay, bananas'}])
+    .get(`/api/v1/form/languages`)
+    .reply(200, [{data: 'fetched form data'}])
+    .get(`/api/v1/form/needs`)
+    .reply(200, [{data: 'fetched form needs'}])
 
   const dispatch = jest.fn()
 
-  return actions.fetchPosts('bananas')(dispatch)
+  return actions.fetchFormDatas('form data')(dispatch)
     .then(() => {
       expect(dispatch.mock.calls.length).toBe(2)
-      expect(dispatch.mock.calls[0][0].type).toBe('REQUEST_POSTS')
-      expect(dispatch.mock.calls[1][0].type).toBe('RECEIVE_POSTS')
+      expect(dispatch.mock.calls[0][0].type).toBe('RECEIVE_LANGUAGES_LIST')
+      expect(dispatch.mock.calls[1][0].type).toBe('RECEIVE_NEEDS_N_SUPPORTS_LIST')
       scope.done()
     })
 })
+
+test('fetchProfiles', () => {
+  const scope = nock('http://localhost')
+    .get(`/api/v1/user/profiles`)
+    .reply(200, [{data: 'fetched profiles'}])
+
+  const dispatch = jest.fn()
+
+  return actions.fetchProfiles('profile data')(dispatch)
+    .then(() => {
+      expect(dispatch.mock.calls.length).toBe(1)
+      scope.done()
+    })
+})
+
+
+
+
+
