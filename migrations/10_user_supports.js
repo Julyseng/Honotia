d353383462c59e68785429fbab680b5beaac37a1
chloe <chloeseunpark@gmail.com>
2019-10-02 17:24:55 +1300

    about page wip

--- migrations/10_user_supports.js
+++ migrations/10_user_supports.js
@@ -1,11 +1,10 @@
-
 exports.up = function(knex) {
-    return knex.schema.createTable('user_support', t => {
-        t.integer('user_id').references('users.id')
-        t.string('supports_id').references('supports.id')
-    })
-};
+  return knex.schema.createTable('user_support', t => {
+    t.integer('user_id').references('users.id')
+    t.integer('supports_id').references('supports.id')
+  })
+}
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('user_support')
-};
+  return knex.schema.dropTable('user_support')
+}
