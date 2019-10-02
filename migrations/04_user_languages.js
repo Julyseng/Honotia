d60d119f8ab41f0700e1134ec7152caa665971aa
chloe <chloeseunpark@gmail.com>
2019-10-02 17:43:52 +1300

    deploy wip

--- migrations/04_user_languages.js
+++ migrations/04_user_languages.js
@@ -1,11 +1,10 @@
-
 exports.up = function(knex) {
-    return knex.schema.createTable('user_languages', t =>{
-        t.integer('user_id').references('users.id')
-        t.integer('language_id').references('languages.id')
-    })
-};
+  return knex.schema.createTable('user_languages', t => {
+    t.integer('user_id').references('users.id')
+    t.integer('language_id').references('languages.id')
+  })
+}
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('user_languages')
-};
+  return knex.schema.dropTable('user_languages')
+}
